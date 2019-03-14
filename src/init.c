5029be6348277762a337345fd3ddc63c14dc8102
Mohamed DCHANE <mdchane@e2r7p11.42.fr>
2019-03-14 14:28:39 +0100

    parsing to be done

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: mdchane <mdchane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/13 11:39:39 by mdchane           #+#    #+#             */
-/*   Updated: 2019/03/14 14:06:01 by mdchane          ###   ########.fr       */
+/*   Updated: 2019/03/14 14:25:16 by mdchane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,7 @@ void	parsing(t_env *e)
 	int		step;
 	char	*line;
 
-	step = 0;
-	while (get_next_line(stdin, &line) > 0)
-	{
-		if (step == 0)
-			parse_nb_ants();
-		else if (step == 1)
-			parse_rooms();
-		else
-			parse_tube();
-		free(line);
-	}
+	parse_nb_ants(e);
+	parse_rooms(e);
+	parse_tube(e);
 }
