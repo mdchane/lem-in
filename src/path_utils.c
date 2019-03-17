5ccec997da6e5847bfc7653176d95a152a701750
Mohamed DCHANE <mdchane@e2r9p8.42.fr>
2019-03-17 12:18:16 +0100

    trying to delete doubles path

--- src/path_utils.c
+++ src/path_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   path_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sarobber <sarobber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mdchane <mdchane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 17:06:16 by sarobber          #+#    #+#             */
-/*   Updated: 2019/03/17 11:20:29 by sarobber         ###   ########.fr       */
+/*   Updated: 2019/03/17 11:29:41 by mdchane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ void			create_path(char **split, t_env *env)
 	t_graph		*graph;
 
 	graph = graph_search(env->graph, split[0]);
-	if (graph)
+	if (graph )
 		path_push_back(&graph->path, path_new(graph_search(env->graph, split[1])));
 	graph = graph_search(env->graph, split[1]);
 	if (graph)
