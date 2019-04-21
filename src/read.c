cca0250afa57730341a478f2cf9753319963ca40
Mohamed DCHANE <mdchane@e2r6p13.42.fr>
2019-04-21 17:29:43 +0200

    gere dev/random

--- src/read.c
+++ src/read.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   read.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sarobber <sarobber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mdchane <mdchane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/13 11:39:39 by mdchane           #+#    #+#             */
-/*   Updated: 2019/04/17 12:47:44 by sarobber         ###   ########.fr       */
+/*   Updated: 2019/04/21 17:26:23 by mdchane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	read_map(t_env *e)
 	while ((ret = read(0, tmp, BUFF_SIZE)) > 0)
 	{
 		tmp[ret] = '\0';
-		if (!ft_isdigit(tmp[0]) && tmp[0] != '#')
+		if (!ft_isascii(tmp[0]))
 			error("ERROR\n");
 		e->len_map += ret;
 		e->buff = ft_strjoinfree(e->buff, tmp);
